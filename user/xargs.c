17aaa351812821526df1cf75599951fc0f91c335
ItamarACGN <Itamarnir10@gmail.com>
2025-06-29 17:43:35 +0300

    Update xargs.c

--- user/xargs.c
+++ user/xargs.c
@@ -6,18 +6,29 @@ int main(int argc, char* argv[]){
     static char line[512];
     char* new_argv[MAXARG+2];
 
+    if(argc > MAXARG){
+        printf("too many arguments\n");
+        exit(0);
+    }
+
     for(int j = 0; j+1 < argc; j++)new_argv[j] = argv[j+1];
     new_argv[argc] = 0;
 
     while(read(0,&line[i],1) > 0){
         if(line[i] != '\n'){
             i++;
+            if(i >= 512){
+                printf("line too long\n");
+                exit(0);
+            }
         }else{
             if(fork() > 0){
+
                 memset(line,0,512);
                 i=0;
                 wait(0);
             }else{
+                
                 line[i] = '\0';
                 new_argv[argc-1] = line;
                 exec(new_argv[0],new_argv);
